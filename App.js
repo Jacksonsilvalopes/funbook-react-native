225affd46b897b28435d541ce699b346a950a5f2
Jackson Silva Lopes <j.a.ckson@hotmail.com>
2025-02-06 03:53:34 -0300

    Implementado Icones e Logica para Dinamica de apresentação  na Tab Bottom

--- App.js
+++ App.js
@@ -10,21 +10,50 @@ import { Conversations } from "./src/surfaces/Conversations";
 import { AddPost } from "./src/surfaces/AddPost";
 import { Favorites } from "./src/surfaces/Favorites";
 import { Profile } from "./src/surfaces/Profile";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Stack = createNativeStackNavigator();
-  // Atualizado
+// Atualizado
 const Tab = createBottomTabNavigator();
 
 function Home() {
   return (
     // Atualizado
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Feed") {
+            iconName = focused ? "md-home" : "md-home-outline";
+          }
+          else
+            if (route.name === "Conversations") {
+              iconName = focused ? "chatbox" : "chatbox-outline";
+            }
+            else
+              if (route.name === "Favorites") {
+                iconName = focused ? "heart" : "heart-outline";
+              }
+              else
+                if (route.name === "Profile") {
+                  iconName = focused ? "person-circle" : "person-circle-outline";
+                }
+          return <Ionicons name={iconName} size={size} color={color} />;
+
+        },
+        tabBarActiveTintColor: "#25A0B0",
+        tabBarActiveTintColor: "#000000",
+      }
+
+
+      )}
+    >
       <Tab.Screen name="Feed" component={Feed} />
       <Tab.Screen name="Conversations" component={Conversations} />
       <Tab.Screen name="AddPost" component={AddPost} />
       <Tab.Screen name="Favorites" component={Favorites} />
       <Tab.Screen name="Profile" component={Profile} />
-    
+
     </Tab.Navigator>
   );
 }
@@ -44,6 +73,6 @@ export default function App() {
           />
         )}
       </Stack.Navigator>
-      </NavigationContainer>
+    </NavigationContainer>
   );
 }
